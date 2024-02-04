cb4796f48eb3244f72d7f0dab450a8b88ce4f75b
KhiulAakash <khiulaakash123@gmail.com>
2024-02-04 16:47:40 +0545

    final commit

--- Day3/app.js
+++ Day3/app.js
@@ -1,1 +1,12 @@
-console.log("Hello! Puja Khiul")
+const data=require("./data")
+const http = require("http");
+
+http
+  .createServer((res, resp) => {
+    resp.writeHead(200, { "Content-Type": "applicationjson" });
+    resp.write(
+      JSON.stringify(data)
+    );
+    resp.end();
+  })
+  .listen(4500);
